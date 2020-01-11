b573e29a591ccdfaf6399e710bb7cb0418b88164
claire froelich <claire.froelich@gmail.com>
2020-01-11 11:57:24 -0500

    add workspace panel

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import MessageList from '../containers/message_list';
 import ChannelList from '../containers/channel_list';
+import WorkspaceList from '../components/workspace_list';
 
 const App = () => {
   return (
     <div className="app">
+      <WorkspaceList />
       <ChannelList />
       <MessageList />
     </div>
---
+++ src/components/workspace_list.jsx
@@ -0,0 +1,12 @@
+// static list for looks only
+import React from 'react';
+
+const WorkspaceList = () => {
+  return (
+    <div className="workspace-list">
+      <img src="../assets/images/dark-wagon.png" alt="workspace icon" className="workspace" />
+    </div>
+  );
+};
+
+export default WorkspaceList;
